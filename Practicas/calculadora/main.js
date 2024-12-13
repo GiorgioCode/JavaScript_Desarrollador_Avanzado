ccf2abdd884fd3379afc08e6fa3699a2d4c9398c
Jorge Angel PAEZ <83848476+GiorgioCode@users.noreply.github.com>
2024-12-12 21:45:06 -0300

    actualizacion practicas

---
+++ Practicas/calculadora/main.js
@@ -0,0 +1,12 @@
+let pantalla = document.getElementById("valor_pantalla");
+
+function mostrar(val) {
+    pantalla.value += val;
+}
+function limpiar_pantalla() {
+    pantalla.value = "";
+}
+
+function evaluar() {
+    pantalla.value = eval(pantalla.value).toFixed(1);
+}
